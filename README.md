7796a0c81f49177ad5fa68c0c91e0808bc2fca59
Pleasant-tech <dummyware2020@gmail.com>
2021-01-01 13:30:30 +0100

    Edited README

---
+++ README.md
@@ -0,0 +1,7 @@
+Store buckets of cli commands and run them with a custom keyword of your choice.
+
+Installation - Pip install buck 🗑️
+
+Support my YouTube channel 
+👇
+youtube.com/c/Pleasanttech
--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup,find_packages
 
+with open("README.md","r") as f:
+  README = f.read()
+  f.close()
+
+
 setup (
   name = 'buck',
   version = '0.0.1',
   description = ' Get started with your projects faster',
-  long_description = 'Store buckets of cli commands and run them with a custom keyword of your choice.',
+  long_description = README,
   long_description_content_type="text/markdown",
+  url = 'https://github.com/Pleasant-tech/Buck/',
   packages = find_packages(),
   include_package_data = True,
+  license="MIT",
+  classifiers =[
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+  ],
   entry_points ='''
     [console_scripts]
-    buck = buck.src.cli:main
+    buck = buck.src.__main__:main
   ''',
   author = 'Pleasant Tech',
   author_email = 'dummyware2020@gmail.com'
